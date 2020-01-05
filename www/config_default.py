cc59a213b0ec1b783c0a66f4324c7eb2f1255606
yiluxiangbei000 <913311670@qq.com>
2020-01-05 21:56:43 +0800

    20200105

---
+++ www/config_default.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'David Yang'
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': 'password',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
